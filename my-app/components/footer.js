cfe6fc336a237c68c53d82030e52c1aaffcb2106
Connor-Kennedy <43087696+Connor-Kennedy@users.noreply.github.com>
2022-05-08 18:44:33 +0100

    WIP footer

--- my-app/components/footer.js
+++ my-app/components/footer.js
@@ -5,16 +5,25 @@ export default function Footer(){
     return (
     <>
     <div className={styles.footer}>
+        <h1 className={styles.lfwHeader}>I'm currently looking for work..</h1>
         <div className={styles.container}>
-            <h2 className={styles.contactMeHeader}>Contact Me</h2>
-            <div>
-                <input className={styles.contactMeName} type="text" placeholder="Name" required="required"></input>
+            <div className={styles.contactMeContainer}>
+                <h2 className={styles.header}>Contact Me</h2>
+                <div>
+                    <input className={styles.contactMeName} type="text" placeholder="Name" required="required"></input>
+                </div>
+                <div>
+                    <input className={styles.contactMeEmail} type="text" placeholder="Email" required="required"></input>
+                </div>
+                <div>
+                    <textarea className={styles.contactMeMessage}  type="text" placeholder="Message" required="required"></textarea>
+                </div>
             </div>
-            <div>
-                <input className={styles.contactMeEmail} type="text" placeholder="Email" required="required"></input>
-            </div>
-            <div>
-                <textarea className={styles.contactMeMessage}  type="text" placeholder="Message" required="required"></textarea>
+            <div className={styles.supportMeContainer}>
+                <h2 className={styles.header}>Support Me</h2>
+                <ul className={styles.servicesList}>
+                    <li>Check out my online stores:</li>
+                </ul>
             </div>
         </div>
     </div>
